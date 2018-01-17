9603ac4273616675d0bbbc05cfe0dce1b8d3eaa9
Sean Guan <zhxiang.guan@gmail.com>
2018-01-17 16:01:02 +0800

    Frontend App: Create project and Create smoke-test.js

---
+++ shop/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h2>Test</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
