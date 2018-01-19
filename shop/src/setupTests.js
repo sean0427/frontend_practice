ab1f566922dc831d0768031761b0ea6203ac6229
Sean Guan <zhxiang.guan@gmail.com>
2018-01-19 18:56:20 +0800

    Test Setup file: create file, write enzyme configure. The file is defind by create-react-app package.

---
+++ shop/src/setupTests.js
@@ -0,0 +1,10 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+/**
+ * Test Setup file
+ * defind by create-react-app 
+ * @see initializing test {@link https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md#initializing-test-environment}
+ **/
+
+Enzyme.configure({ adapter: new Adapter() });
