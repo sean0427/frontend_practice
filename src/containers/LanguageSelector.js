f1ba5e35ef6c247f87e5cf391956f563d83ed599
Sean Guan <zhxiang.guan@gmail.com>
2018-02-25 15:17:31 +0800

    Container: fix wrong implement way.

--- src/containers/LanguageSelector.js
+++ src/containers/LanguageSelector.js
@@ -1,21 +1,16 @@
-import React from 'react';
 import { connect } from 'react-redux';
 import Selector from '../components/Selector';
 
-const mapStateToProps = (state, _) => ({
-    language: state.language,
-});
-
+const NAME = 'language';
 const LANGUAGE_CODE = 'code';
 const LANGUAGE_NAME = 'name';
 
-const LanguageSelector = ({language}) => (
-    <Selector
-        name="language"
-        textName={LANGUAGE_NAME}
-        valueName={LANGUAGE_CODE}
-        options={language}
-    />
-);
+const mapStateToProps = (state, _) => ({
+    name: NAME,
+    textName: LANGUAGE_NAME,
+    valueName: LANGUAGE_CODE,
+    options: state.language,
+});
 
-export default connect(mapStateToProps)(LanguageSelector);
+
+export default connect(mapStateToProps)(Selector);
--- src/containers/ProductTypeSelector.js
+++ src/containers/ProductTypeSelector.js
@@ -1,20 +1,13 @@
-import React from 'react';
 import { connect } from 'react-redux';
 import Selector from '../components/Selector';
-
-const mapStateToProps = (state, _) => ({
-    productType: state.products.type,
-});
 
 const PRODUCT_TYPE_ID = 'id';
 const PRODUCT_TYPE_NAME = 'name';
 
-const ProductTypeSelector = ({productType}) => (
-    <Selector
-        textName={PRODUCT_TYPE_NAME}
-        valueName={PRODUCT_TYPE_ID}
-        options={productType}
-    />
-);
+const mapStateToProps = (state, _) => ({
+    textName: PRODUCT_TYPE_NAME,
+    valueName: PRODUCT_TYPE_ID,
+    options: state.products.type,
+});
 
-export default connect(mapStateToProps)(ProductTypeSelector);
+export default connect(mapStateToProps)(Selector);
